2859254c4e217c035cdbc996d70beccad3fdd4f6
luis- <luis-@LAPTOP-001>
2022-03-11 18:03:55 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Holamundo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Views/MyController/MyControllerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Holamundo.Views.MyController
+{
+    public class MyControllerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Captura()
+        {
+            return View();
+        }
+    }
+
+}
